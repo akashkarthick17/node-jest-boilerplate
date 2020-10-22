faa04b3ef5ba08f9aa2e821524aa54cd2c6cfc1c
supriyadi687 <agus@durian.money>
2020-10-22 11:19:28 +0000

    bayu waskita

--- src/stringcalculator.js
+++ src/stringcalculator.js
@@ -1,3 +1,46 @@
-module.exports = (numbers) => {
-    return 0;
+module.exports = (input) => {
+    const data = checkDelimeter(input)
+    
+    const numbersArr = data.inputString.split(data.delimeter)
+    const negatives = []
+    const res = calculate(numbersArr)
+    if (res.negatives.length > 0) {
+        const err = "negatives not allowed: "+res.negatives.join(", ")
+        return err
+    }
+    return res.total
+}
+
+const checkDelimeter = (input) => {
+    let delimeter = /,|\n/
+    let inputString = input
+    if (input.substring(0,2) == "//") {
+        const dataString = input.split("\n")
+        inputString = dataString[1]
+        delimeter = dataString[0].substring(2, dataString[0].length)
+    }
+
+    return {delimeter, inputString}
+}
+
+const calculate = (numbersArr) => {
+    const negatives = []
+    let total = 0
+    for (let i = 0; i < numbersArr.length; i++) {
+        if (numbersArr[i] == "") {
+            continue
+        }
+        const number = parseInt(numbersArr[i])
+        if (number < 0) {
+            negatives.push(number)
+            continue
+        }
+
+        if (number > 1000) {
+            continue
+        }
+
+        total = total + number
+    }
+    return {negatives, total}
 }
--- src/stringcalculator.test.js
+++ src/stringcalculator.test.js
@@ -11,4 +11,64 @@ describe('add()', () => {
 
     expect(actual).toEqual(expected)
   })
+
+  test('Given input 1,2, should returns 3', () => {
+    const given = "1,2"
+
+    const expected = 3;
+
+    const actual = add(given)
+
+    expect(actual).toEqual(expected)
+  })
+
+  test('Given input 1,2,3,4, should returns 10', () => {
+    const given = "1,2,3,4"
+
+    const expected = 10;
+
+    const actual = add(given)
+
+    expect(actual).toEqual(expected)
+  })
+
+  test('Given input 1\n2,3, should returns 6', () => {
+    const given = "1\n2,3"
+
+    const expected = 6;
+
+    const actual = add(given)
+
+    expect(actual).toEqual(expected)
+  })
+
+  test('Given input //;\n1;2, should returns 3', () => {
+    const given = "//;\n1;2"
+
+    const expected = 3;
+
+    const actual = add(given)
+
+    expect(actual).toEqual(expected)
+  })
+
+  test('Given input 1,4,-1, should returns negatives not allowed: -1', () => {
+    const given = "1,4,-1"
+
+    const expected = "negatives not allowed: -1";
+
+    const actual = add(given)
+
+    expect(actual).toEqual(expected)
+  })
+
+  test('Given input 2,1001, should returns 2', () => {
+    const given = "2,1001"
+
+    const expected = 2;
+
+    const actual = add(given)
+
+    expect(actual).toEqual(expected)
+  })
 })
