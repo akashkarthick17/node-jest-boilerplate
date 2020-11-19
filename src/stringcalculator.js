0da2a8dfb72186d66601ef23456adb059312f207
Akash Karthick <akarthick@nvenergy.com>
2020-11-19 14:13:38 +0530

    Completed 6 steps

--- src/stringcalculator.js
+++ src/stringcalculator.js
@@ -1,3 +1,77 @@
-module.exports = (numbers) => {
-    return 0;
+function getSumOfArray(arr) {
+  let sum = 0;
+  let negativeNumbers = '';
+  arr.forEach((number) => {
+    if (+number < 0) {
+      negativeNumbers += number;
+    } else if (number > 1000) {
+      sum += 0;
+    } else {
+      sum += +number;
+    }
+  });
+
+  return { sum, negativeNumbers };
 }
+
+function getNewLineIndex(numbers) {
+  return numbers.indexOf('\n');
+}
+
+function getNumbersWithoutDelimiter(numbers, newLineIndex) {
+  return numbers.substr(newLineIndex);
+}
+
+function getDelimiter(numbers, newLineIndex) {
+  return numbers.substring(2, newLineIndex);
+}
+
+function calculateSum(numbers, delimiter) {
+  let numArr = numbers.split(',');
+  numArr = numArr.join(' ').split('\n');
+  if (delimiter) {
+    numArr = numArr.join(' ').split(delimiter);
+  }
+  numArr = numArr.join(' ');
+  numArr = numArr.split(' ');
+
+  const { sum, negativeNumbers } = getSumOfArray(numArr);
+
+  if (negativeNumbers.length > 0) {
+    throw Error('negatives not allowed: ' + negativeNumbers);
+  } else {
+    return sum;
+  }
+}
+
+function extractErrorMessage(error) {
+  return error.toString().split('Error: ')[1];
+}
+
+function getNumbersAndDelimiter(numbers) {
+  const newLineIndex = getNewLineIndex(numbers);
+  const delimiter = getDelimiter(numbers, newLineIndex);
+  const numbersWithoutDelimiter = getNumbersWithoutDelimiter(
+    numbers,
+    newLineIndex
+  );
+
+  return { numbersWithoutDelimiter, delimiter };
+}
+
+module.exports = (numbers) => {
+  let sum = 0;
+  try {
+    if (numbers.startsWith('//')) {
+      const { numbersWithoutDelimiter, delimiter } = getNumbersAndDelimiter(
+        numbers
+      );
+      sum = calculateSum(numbersWithoutDelimiter, delimiter);
+    } else {
+      sum = calculateSum(numbers);
+    }
+    return sum;
+  } catch (error) {
+    return extractErrorMessage(error);
+  }
+};
--- src/stringcalculator.test.js
+++ src/stringcalculator.test.js
@@ -1,14 +1,86 @@
 /* eslint-env jest */
-const add = require('./stringcalculator')
+const add = require('./stringcalculator');
 
 describe('add()', () => {
   test('Given empty string, should returns 0', () => {
-    const given = ""
+    const given = '';
 
     const expected = 0;
 
-    const actual = add(given)
+    const actual = add(given);
 
-    expect(actual).toEqual(expected)
-  })
-})
+    expect(actual).toEqual(expected);
+  });
+
+  test('Given a string with one number, should return the exact number', () => {
+    const given = '1';
+
+    const expected = 1;
+
+    const actual = add(given);
+
+    expect(actual).toEqual(expected);
+  });
+
+  test('Given a string with two number, should return the sum of two numbers', () => {
+    const given = '1,2';
+
+    const expected = 3;
+
+    const actual = add(given);
+
+    expect(actual).toEqual(expected);
+  });
+
+  test('Given a string with two number, should return the sum of two numbers', () => {
+    const given = '3,5,7,2,8,1,10,45';
+
+    const expected = 81;
+
+    const actual = add(given);
+
+    expect(actual).toEqual(expected);
+  });
+
+  test('Given a string with two number along with delimiter, should return the sum of the numbers', () => {
+    const given = '1\n2,3';
+
+    const expected = 6;
+
+    const actual = add(given);
+
+    expect(actual).toEqual(expected);
+  });
+
+  test('Given a string with two number along with custom delimiter, should return the sum of the numbers', () => {
+    // const given = '//;\n1;2;3';
+    // const given = '//###\n1###2###3';
+    const given = '//++\n1++2++3';
+
+    const expected = 6;
+
+    const actual = add(given);
+
+    expect(actual).toEqual(expected);
+  });
+
+  test('Given a string with negative numbers, should throw an exception', () => {
+    const given = '1,4,-1';
+
+    const expected = 'negatives not allowed: -1';
+
+    const actual = add(given);
+
+    expect(actual).toEqual(expected);
+  });
+
+  test('Given a string with big numbers, should be ignored', () => {
+    const given = '1,2,3,1001';
+
+    const expected = 6;
+
+    const actual = add(given);
+
+    expect(actual).toEqual(expected);
+  });
+});
